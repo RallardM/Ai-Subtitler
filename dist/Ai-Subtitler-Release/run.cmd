879dc7eb821e23c8b320a1f283478734c28419df
remi.allard-mayer <remi.allard-mayer@outlook.com>
2026-02-20 19:17:15 -0500

    Enhance run.cmd to support configuration for Debug and Release builds

--- dist/Ai-Subtitler-Release/run.cmd
+++ dist/Ai-Subtitler-Release/run.cmd
@@ -1,14 +1,39 @@
 @echo off
 setlocal
 
+REM If both Release and Debug exist, you can force which one is used:
+REM   set AI_SUBTITLER_CONFIG=Debug
+REM   .\start-ai-subtitler.cmd 0
+set "PREFER=%AI_SUBTITLER_CONFIG%"
+
 set "EXE=%~dp0ai-subtitler-streamerbot.exe"
 
+if exist "%EXE%" goto have_exe
+
+if /I "%PREFER%"=="Debug" goto prefer_debug
+if /I "%PREFER%"=="Release" goto prefer_release
+
+:prefer_release
+set "EXE=%~dp0build\Release\ai-subtitler-streamerbot.exe"
+if exist "%EXE%" goto have_exe
+
+set "EXE=%~dp0build\Debug\ai-subtitler-streamerbot.exe"
+if exist "%EXE%" goto have_exe
+goto check_bin
+
+:prefer_debug
+set "EXE=%~dp0build\Debug\ai-subtitler-streamerbot.exe"
 if exist "%EXE%" goto have_exe
 
 set "EXE=%~dp0build\Release\ai-subtitler-streamerbot.exe"
 if exist "%EXE%" goto have_exe
 
+:check_bin
+
 set "EXE=%~dp0build\bin\Release\ai-subtitler-streamerbot.exe"
+if exist "%EXE%" goto have_exe
+
+set "EXE=%~dp0build\bin\Debug\ai-subtitler-streamerbot.exe"
 if exist "%EXE%" goto have_exe
 
 if not exist "%EXE%" (
@@ -16,6 +41,7 @@ if not exist "%EXE%" (
   echo Build it first:
   echo   cmake -S . -B build -A x64
   echo   cmake --build build --config Release
+  echo   cmake --build build --config Debug
   exit /b 1
 )
 
